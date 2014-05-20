57cc64335b24dd2c03cd858731e302778fd78434
Debellatrix <vlatka.pavisic@gmail.com>
2014-05-20 20:13:30 +0200

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ Nikofe::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -30,3 +32,6 @@ Nikofe::Application.configure do
 
   Paperclip.options[:command_path] = "/usr/local/bin/"
 end
+
+#letter opener
+#mandrillapp.com 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,18 @@ Nikofe::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'example.com' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 25,
+    :user_name => "vlatka.pavisic@gmail.com",
+    :password  => "cgjTSUK5RGlLRKyIKxyiLg"
+  }
 end
