23a1de2fe96ecf729fa4507c5a0d575470b5eaeb
Debellatrix <vlatka.pavisic@gmail.com>
2014-05-20 15:55:40 +0200

    Fix issue with multiple signed-in users...

--- app/controllers/cms_pages_controller.rb
+++ app/controllers/cms_pages_controller.rb
@@ -1,43 +1,32 @@
 class CmsPagesController < ApplicationController
 	layout "website_layout", only: [ :website_preview, :show_page ]
 	before_action :authenticate_user!, only: [ :website_structure, :home ]
-	before_action :get_data, only: [:website_structure, :website_preview, :show_page]
 
 	def home
 		@website = current_user.website
 	end
 
 	def website_structure
+		if @website = current_user.website
+			@pages = @website.pages
+			@navbar_items = @website.navbar_items.order(:created_at)
+			@independent_pages = @pages.where("navbar_item_id is NULL").order(:created_at)
+		end
 	end
 
 	def website_preview
+		@website = Website.find_by(slug: params[:website_slug])
+		@pages = @website.pages 
+		@navbar_items = @website.navbar_items.order(:created_at)
+		@independent_pages = @pages.where(["navbar_item_id is NULL and website_id = ?", @website.id]).order(:created_at)
 	end
 
 	def show_page
-		if user_signed_in?
-			@page = current_user.pages.find_by(slug: params[:page_slug])
-		else
-			@website = Website.find_by(slug: params[:website_slug])
-			@page = Page.where("website_id = ? and slug = ?", @website.id, params[:page_slug]) #sve prebaciti u where
-		end
-	end
-
-	private
-	
-	def get_data
-		if user_signed_in?
-			if current_user.website
-				@website = current_user.website
-				@pages = @website.pages
-				@navbar_items = @website.navbar_items.order(:created_at)
-				@independent_pages = @pages.where("navbar_item_id is NULL").order(:created_at)
-			end
-		else
-			@website = Website.find_by(slug: params[:website_slug])
-			@pages = @website.pages 
-			@navbar_items = @website.navbar_items.order(:created_at)
-			@independent_pages = @pages.where(["navbar_item_id is NULL and website_id = ?", @website.id]).order(:created_at)
-		end
+		@website = Website.find_by(slug: params[:website_slug])
+		@pages = @website.pages 
+		@navbar_items = @website.navbar_items.order(:created_at)
+		@independent_pages = @pages.where(["navbar_item_id is NULL and website_id = ?", @website.id]).order(:created_at)
+		@page = Page.where("website_id = ? and slug = ?", @website.id, params[:page_slug]).first #sve prebaciti u where
 	end
 
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
 	before_action :get_website, except: [:new, :create]
 
-  protected
-
-    def after_update_path_for(resource)
-      cms_home_path
-    end
-
-    def get_website
-    	@website = current_user.website 
-    end
 end
