56cffcbd0dfdaa352ae5dae4a8c994f7cf7906cf
Debellatrix <vlatka.pavisic@gmail.com>
2014-06-03 22:38:28 +0200

    Add Dropbox storage for Paperclip

--- app/models/website.rb
+++ app/models/website.rb
@@ -2,7 +2,10 @@ class Website < ActiveRecord::Base
 	belongs_to :user
 	has_many :pages
 	has_many :navbar_items
-	has_attached_file :home_cover
+	has_attached_file :home_cover,
+		:storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :dropbox_options => {}
 	validates :name, uniqueness: { case_sensitive: false }
 	validates_attachment_content_type :home_cover, :content_type => /\Aimage/
 	validates_attachment_file_name :home_cover, :matches => [/png\Z/, /jpe?g\Z/]
