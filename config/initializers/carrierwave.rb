66e0f7dfde53a49ccfe70393943b14abbc3ec0cf
Debellatrix <vlatka.pavisic@gmail.com>
2014-06-07 18:14:35 +0200

    Finalization

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  config.dropbox_app_key = "utavqq18ugdcbhh"
-  config.dropbox_app_secret = "3atvdwacu8lqhs3"
-  config.dropbox_access_token = "qj0v9qyzi7xj2tg5"
-  config.dropbox_access_token_secret = "2fu6kpcvwyrbe34"
-  config.dropbox_user_id = "167552825"
-  config.dropbox_access_type = "app_folder"
-end
